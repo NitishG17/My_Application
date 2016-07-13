4f61a580e4c34e3e34f6844ce8efc95ee420bfa8
Nitish.Gaikwad <nitish.gaikwad@dmartindia.com>
2016-07-13 09:49:38 +0530

    1st Change

--- app/src/main/java/com/example/nitishgaikwad/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nitishgaikwad/myapplication/MainActivity.java
@@ -19,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                
+                tv.setText("test");
+
             }
         });
     }
